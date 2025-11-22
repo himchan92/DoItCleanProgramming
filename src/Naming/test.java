b780854c5dc89824cc9bedc41a49ec7bf3029528
user <thdjun92@naver.com>
2025-11-22 14:55:42 +0900

    init

---
+++ src/Naming/test.java
@@ -0,0 +1,13 @@
+package Naming;
+
+public class test {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        System.out.println("SUM : " + getV(a, b));
+    }
+
+    static public int getV(int a, int b) {
+        return a + b;
+    }
+}
--- src/Refactoring/Student.java
+++
@@ -1,9 +0,0 @@
-package Refactoring;
-
-public class Student {
-    int score;
-
-    public Student(int score) {
-        this.score = score;
-    }
-}
